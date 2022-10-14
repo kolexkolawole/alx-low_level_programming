5d24c1da6f90490af7258137f581198d3e0eabc2
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-13 22:04:04 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
  *
  * main - Serves as the entry point for the program
  *  
- * Return: By default returns Zero(0) if no errors otherwise returns non-zero value. (Success)
-*
+ * Return: By default returns 0 if no errors otherwise returns non-zero value. (Success)
 */
 
 int main(void)
