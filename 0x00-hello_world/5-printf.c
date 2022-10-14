8288f3d21ab641fa8b8437afb1820fc6c816681f
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-13 21:52:43 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,20 +2,18 @@
 
 /**
  *
- *  * main-entry point for the program.
+ * main-entry point for the program.
  *
- *   *
+ * Return:0 if no errors, return non zero if errors
  *
- *    * Return:0 if no errors, return non zero if errors
- *
- *     */
+ */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
-			return (0);
+return (0);
 
 }
