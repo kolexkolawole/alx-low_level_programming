eea842f04f9d15a4ed7b9c2893e651facd257c84
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-13 21:36:17 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,20 +2,17 @@
 
 /**
 
- *main-program entry point.
+*main-program entry point.
 
-  *
+*
 
-   *Return: 1 on success.
+*Return: 1 on success.
 
-    */
+*/
 
-    int main(void)
+int main(void)
 
-    {
-
-	    	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
-		}
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
