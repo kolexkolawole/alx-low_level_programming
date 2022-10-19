6297496b87e30e2864b4af5679eab63a5cbbbc18
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-19 07:17:33 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,9 @@
  *
  * main - Serves as the entry point for the program
  *  
- * Return: By default returns 0 if no errors otherwise returns  * non-zero value. (Success)
+ * Return: By default returns 0 if no errors otherwise returns  * non-zero value  (Success)
 */
 
 int main(void)
 
-{
-puts("\"Programming is like building a multilingual puzzle");  return (0);
-
-}
+{puts("\"Programming is like building a multilingual puzzle"); return (0);}
---
+++ README.md
@@ -0,0 +1,1 @@
+writing 0x00.c - hello, world
