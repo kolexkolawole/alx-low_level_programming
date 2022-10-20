16e46c182fbdd7c996c467d55820fc861c9b35b5
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-20 06:53:21 -0700

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,12 @@
 
 /**
  *
- *  *main -is the entry point for the program.
+ * main -is the entry point for the program.
  *
- *    *
- *
- *    *Return:zero if no error, Non-zero value  if errors.
- *
+ * Return: zero if no error, Non-zero value  if errors.
  */
 
 int main(void)
-
 {
 
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
