8ed165bbc39009ed2d115a17405c8e82fbc18577
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-23 16:19:01 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
 /**
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints _putchar and a new line
+*
+* Return: Always 0
+*/
+
 int main(void)
 {
-		int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-		int count, sz;
+	_putchar('_');
 
-			sz = sizeof(str) / sizeof(int);
-			for (count = 0; count < sz; count++);
-{
-			_putchar(str[count]);									_putchar('\n');	
-}
-			return (0);
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
 }
