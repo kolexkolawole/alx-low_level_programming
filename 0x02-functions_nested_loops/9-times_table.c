1118cb530e03978eeab3d09fa8264552bed315b3
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-24 11:36:56 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,25 +12,23 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		_putchar(48);
+		_putchar('0');
 		for (b = 1; b <= 9; b++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			op = a * b;
-			_putchar(44);
-			_putchar(32);
 
-				if (op <= 9)
-
-{
-
-			_putchar(32);
-
-			_putchar(op + 48);
-		}
+			if (op <= 9)
+			{
+			_putchar(' ');
+			_putchar(op + '0');
+			}
 			else
 			{
-				_putchar((op / 10) + 48);
-				_putchar((op % 10) + 48);
+				_putchar((op / 10) + '0');
+				_putchar((op % 10) + '0');
 			}
 		}
 		_putchar('\n');
