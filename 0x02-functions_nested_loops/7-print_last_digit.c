7e860e3f9180e64c4c0623efc6c45eb9b2a719ac
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-24 12:43:57 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,19 +7,17 @@
  */
 int print_last_digit(int n)
 {
-
 	int ld = n % 10;
 
 	if (ld < 0)
-		{
-			ld *= -1;
-			_putchar(ld + '0');
+	{
+		_putchar(-ld + '0');
 			return (-1);
-		}
+	}
 	else
 	{
-		ld *= 1;
+
 		_putchar(ld + '0');
 			return (1);
-		}
+	}
 }
