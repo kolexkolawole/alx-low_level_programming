61f1931a2fe32bf8248881680b0f0871e574429c
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-25 02:54:25 -0700

    commit

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,8 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 
+int _strlen(char *);
+
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -11,7 +12,7 @@ int main(void)
 	char *str;
 	int len;
 
-	str = "My first strlen!";
+	str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 	len = _strlen(str);
 	printf("%d\n", len);
 	return (0);
