d457c357f0299c179240c2bfc296a33d7d576917
kolexkolawole@gmail.com <kolexkolawole@gmail.com>
2022-10-25 02:37:02 -0700

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,24 +5,18 @@
  * @str: input string.
  * Return: no return.
  */
-
 void _puts(char *str)
 {
-		int count = 0;
+	int count = 0;
 
-			while (count >= 0)
-
-			{
-
-	if (str[count] == '\0')
-											
+	while (count >= 0)
 	{
-
-	_putchar('\n');
-																		break;																	}
-		
-	_putchar(str[count]);
-	count++;
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
 	}
 }
-
